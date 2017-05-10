732f9301da6fcffe93cb9d03e9a0e6a09ec8f09a
Liu Peng <lplearn@163.com>
2017-05-10 17:52:47 +0800

    向下转型与运行时类型识别

---
+++ src/com/company/多态/T03.java
@@ -0,0 +1,60 @@
+package com.company.多态;
+
+/**
+ * Created by OKMAN on 2017/5/10.
+ */
+import static net.mindview.util.Print.*;
+class Cycle {
+    private String name = "Cycle";
+    public static void travel(Cycle c) {
+        System.out.println("Cycle.ride() " + c);
+    }
+    public String toString() {
+        return this.name;
+    }
+}
+
+class Unicycle extends Cycle {
+    private String name = "Unicycle";
+    public void balance() { System.out.println("Balance Unicycle"); }
+    public String toString() {
+        return this.name;
+    }
+}
+
+class Bicycle extends Cycle {
+    private String name = "Bicycle";
+    public void balance() { System.out.println("Balance Bicycle"); }
+    public String toString() {
+        return this.name;
+    }
+
+}
+
+class Tricycle extends Cycle {
+    private String name = "Tricycle";
+    public String toString() {
+        return this.name;
+    }
+}
+
+public class T03 {
+    public static void main(String[] args) {
+        Cycle[] ride = {
+                new Unicycle(),
+                new Bicycle(),
+                new Tricycle(),
+        };
+        // Compile time error: cannot find balance() method in Cycle:
+        // for(Cycle c : ride) {
+        //	c.balance();
+        // }
+        ((Unicycle)ride[0]).balance();
+        ((Bicycle)ride[1]).balance();
+        // Compile time error: no balance() in Tricycle:
+        // ((Tricycle)ride[2]).balance();
+        // RTTI: ClassCastException: Tricycle cannot be cast to Bicycle:
+        //((Bicycle)ride[2]).balance();
+    }
+}
+
